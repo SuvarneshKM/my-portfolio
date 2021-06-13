8c1746a9293b13c6854e85c6a282390a93cf70d1
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-06-13 14:19:05 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
   return (
     <div>
       <Main />
+      <center>
+        <div id="webring-wrapper">
+            <a href="https://webring.hackclub.com/" id="previousBtn" class="webring-anchor" title="Previous">‹</a>
+            <a href="https://webring.hackclub.com/" class="webring-logo" title="Hack Club Webring" alt="Hack Club Webring"></a>
+            <a href="https://webring.hackclub.com/" id="nextBtn" class="webring-anchor" title="Next">›</a>
+            <script src="https://webring.hackclub.com/public/embed.min.js"></script>
+        </div>
+    </center>
     </div>
   );
 }
