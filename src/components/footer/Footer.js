ff92ee97c38cc73c703c8587895e02aaec6d3c47
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-07-02 18:39:04 +0530

    hack club

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import "./Footer.scss";
 import { Fade } from "react-reveal";
 import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
+
 export default function Footer() {
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = "https://webring.hackclub.com/public/embed.min.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
   const { isDark } = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
