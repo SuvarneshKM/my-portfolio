468db6fcaa8238e5373baa608b5f71abb3c881d1
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-07-02 18:03:07 +0530

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,11 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Footer.scss";
-import {Fade} from "react-reveal";
+import { Fade } from "react-reveal";
 import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
@@ -19,6 +19,14 @@ export default function Footer() {
           </a>
         </p>
       </div>
+      <center>
+        <div id="webring-wrapper">
+          <a href="https://webring.hackclub.com/" id="previousBtn" class="webring-anchor" title="Previous">‹</a>
+          <a href="https://webring.hackclub.com/" class="webring-logo" title="Hack Club Webring"
+            alt="Hack Club Webring"></a>
+          <a href="https://webring.hackclub.com/" id="nextBtn" class="webring-anchor" title="Next">›</a>
+        </div>
+      </center>
     </Fade>
   );
 }
