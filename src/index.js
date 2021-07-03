3ffa9f324e2b0478680a1a46c8436a6e9a267001
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-07-03 13:40:16 +0530

    redirect router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Redirect from="*" to="/" />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
